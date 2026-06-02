3bcc96068d6b5dde9c71f77c1ec274ba672a3189
thattemperature <2719023332@qq.com>
2026-06-02 20:25:07 +0800

    Change the type of "environment-variables".

--- secrets/environment-variables.nix
+++ secrets/environment-variables.nix
@@ -6,9 +6,13 @@
     with lib;
     with types;
     {
-      thattem.secrets.environment-variables.path = mkOption {
-        type = nullOr str;
-        default = null;
+      thattem.secrets.environment-variables = mkOption {
+        type = attrsOf (submodule {
+          options = {
+            path = mkOption { type = str; };
+          };
+        });
+        default = { };
       };
     };
 
